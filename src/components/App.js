05e4b55a604385f5c12bf93341a165660ded77cb
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-04 03:24:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,9 @@
-
-// import React from "react";
-// import './../styles/App.css';
-
-// const App = () => {
-//   return (
-//     <div>
-//         {/* Do not remove the main div */}
-//     </div>
-//   )
-// }
-
-// export default App
-
+// App.js
 import React, { useState } from "react";
 import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  // Static weather data using useState
   const [weather, setWeather] = useState({
     temperature: 25,
     conditions: "Sunny"
@@ -29,7 +15,6 @@ const App = () => {
       <WeatherDisplay weather={weather} />
     </div>
   );
-}
+};
 
 export default App;
-
